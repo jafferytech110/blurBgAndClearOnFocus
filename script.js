77d48a4ad9c1c4be2e3e66000616fe0627d41290
jafferytech110 <sarfarazalijaffery@gmail.com>
2021-09-27 22:51:16 +0200

    completed

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ $(document).ready(function () {
     $(".mouse").css({ left: left - 150, top: top - 150 });
   });
 });
+
+// document
+//   .querySelector(".mouse_hover_area")
+//   .addEventListener("mousemove", (e) => {
+//     let top = this.scrollTop - this.offset().top + e.clientY;
+//     let left = this.scrollLeft - this.offset().left + e.clientX;
+//     document.querySelector(".mouse").style.left = left - 150;
+//     document.querySelector(".mouse").style.top = top - 150;
+//   });
+
+// $(document).ready(function () {
+//   $(".mouse_hover_area").touchmove(function (e) {
+//     e.preventDefault();
+//     let top = $(window).scrollTop() - $(this).offset().top + e.clientY;
+//     let left = $(window).scrollLeft() - $(this).offset().left + e.clientX;
+//     $(".mouse").css({ left: left - 150, top: top - 150 });
+//   });
+// });
